c8e594eadad93a2f8ff24ba13e1fe5d0d6bfb212
Caio Resende <caioresende@Caios-MacBook-Pro.local>
2018-01-15 17:51:14 -0200

    changes on multiple selection question

--- src/app/review/ReviewController.js
+++ src/app/review/ReviewController.js
@@ -2,13 +2,22 @@ module.exports = function(ReviewService) {
     var self = this;
     this.data = ReviewService;
 
+    this.checkForMaxOptions = function(question, option) {
+        if (question.sel_options == question.max_options && question.answer.indexOf(option) < 0) {
+            return true;
+        }
+        return false;
+    };
+
     this.checkQuestionOptions = function (question, option) {
         var idx = question.answer.indexOf(option);
 
         if (question.answer.length && idx > -1) {
-            question.answer.splice(idx, 1);
+            question.answer = question.answer.replace( ' ' + option, '');
+            question.sel_options--;
         } else {
-            question.answer.push(option)
+            question.answer = question.answer + ' ' + option; 
+            question.sel_options++;
         }
     }
 };
